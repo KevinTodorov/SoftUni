4e7d8fde67b5570d2714a091214386a63af322cc
KevinTodorov <kevintodorov8@gmail.com>
2022-03-03 00:44:37 +0200

    Added new lesson: nestled loops

---
+++ ProgramingBasics witch c#/nestled loops/Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+           int rows = int.Parse(Console.ReadLine());
+
+            for (int floor = floors; floor <= floors; floor--)
+            {
+                if (floor < 1)
+                {
+                    break;
+                }
+                for (int row = 0; row < rows; row++)
+                {
+                    if(floor == floors)
+                    {
+                        Console.Write($"L{floor}{row} ");
+                        
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        Console.Write($"O{floor}{row} ");
+                    }else
+                    {
+                        Console.Write($"A{floor}{row} ");
+                    }
+                    
+                }
+            Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ProgramingBasics witch c#/nestled loops/Number Pyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int curr = 1;
+            bool flag = false;
+
+            for (int r = 1; r <= number; r++)
+            {
+                for (int c = 1; c <= r; c++)
+                {
+                    if(curr > number)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    if (flag)
+                    {
+                        break;
+                    }
+                    Console.Write(curr + " ");
+                    curr++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
