6a03670fca5e34ca816b40467bf80f45208b868d
KevinTodorov <kevintodorov8@gmail.com>
2022-03-07 23:04:20 +0200

    Added new course and first lesson to the course: Intro and Basic Syntax.

---
+++ c# Fundamentals/Intro and Basic Syntax/BackIn30Min/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BackIn30Min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = double.Parse(Console.ReadLine());
+            double minutes = double.Parse(Console.ReadLine());
+            double addedMinutes = minutes + 30;
+
+            if (addedMinutes > 60)
+            {
+                hour += 1;
+
+                if (hour >= 24)
+                {
+                    hour = 0;
+                }
+                addedMinutes -= 60;
+            }
+            if (addedMinutes < 10)
+            {
+                Console.WriteLine($"{hour}:0{addedMinutes}");
+                return;
+            }
+
+            Console.WriteLine($"{hour}:{addedMinutes}");
+        }
+    }
+}
---
+++ c# Fundamentals/Intro and Basic Syntax/SumOfOddNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 1; i <= times; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    times += 1;
+                    sum += i;
+                    Console.WriteLine(i);
+                }               
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
