ee1a429caca67dbdef1cad864219163939c8d4f5
KevinTodorov <kevintodorov8@gmail.com>
2022-03-29 20:01:01 +0300

    Added new lesson: Text processing

---
+++ ProgramingBasics witch c#/TextProcessing/Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            string wordToBeRemoved = Console.ReadLine();
+            string word = Console.ReadLine();
+            int index = word.IndexOf(wordToBeRemoved);
+
+            while (index >= 0)
+            {
+               word = word.Remove(index, wordToBeRemoved.Length);
+               index = word.IndexOf(wordToBeRemoved);
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
