93c3fee9cd9dbd455a32d39979f6bfc0479d5fd7
KevinTodorov <kevintodorov8@gmail.com>
2022-04-01 19:46:30 +0300

    Added new lesson: RegularExpressions

---
+++ c# Fundamentals/RegularExpressions/Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}");
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
---
+++ c# Fundamentals/RegularExpressions/MatchDates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchCollection macthes = Regex.Matches(Console.ReadLine(), @"(?<day>[0-9]{2})(?<separator>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<separator>([0-9]{4})");
+
+            foreach (Match match in macthes)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups[1]}");
+            }
+
+        }
+    }
+}
