ebe7d862417c23f1b64f7383bd22935e469fd01a
KevinTodorov <kevintodorov8@gmail.com>
2022-04-05 18:52:30 +0300

    Added test Exam

---
+++ c# Fundamentals/01.Programming Fundamentals Final Exam Retake/Ad Astra/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Ad_Astra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Regex regex =
+                new Regex(@"(\||\#)(?<name>[a-zA-Z\s]+)\1(?<date>[0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(?<calories>[0-9]+)\1");
+
+            MatchCollection mathingFoods = regex.Matches(text);
+
+            int calories = 0;
+
+            foreach (Match food in mathingFoods)
+            {
+                int caloriesOfCurrentFood = int.Parse(food.Groups["calories"].Value);
+
+                if (caloriesOfCurrentFood <= 10000 && caloriesOfCurrentFood >= 0)
+                {
+                    calories += caloriesOfCurrentFood;
+                }
+            }
+
+            int days = calories / 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match food in mathingFoods)
+            {
+                Console.WriteLine($"Item: {food.Groups["name"].Value}, Best before: {food.Groups["date"].Value}, Nutrition: {food.Groups["calories"].Value}");
+            }
+        }
+    }
+}
---
+++ c# Fundamentals/01.Programming Fundamentals Final Exam Retake/The Imitation Game/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] commandInArray = command.Split("|").ToArray();
+                string action = commandInArray[0];
+
+                switch (action)
+                {
+                    case "Move":
+                    {
+                        int times = int.Parse(commandInArray[1]);
+                        string temp = string.Empty;
+                        StringBuilder sbBuilder = new StringBuilder(encryptedMessage);
+
+                        for (int i = 0; i < times; i++)
+                        {
+                            temp += sbBuilder[0];
+                            sbBuilder.Remove(0, 1);
+                        }
+
+                        sbBuilder.Append(temp);
+                        encryptedMessage = sbBuilder.ToString();
+                    }
+                        break;
+                    case "Insert":
+                    {
+                        int index = int.Parse(commandInArray[1]);
+                        string value = commandInArray[2];
+                        StringBuilder sbBuilder = new StringBuilder(encryptedMessage);
+                        sbBuilder.Insert(index, value);
+                        encryptedMessage = sbBuilder.ToString();
+                    }
+                        break;
+                    case "ChangeAll":
+                    {
+                        string substring = commandInArray[1];
+                        string replacement = commandInArray[2];
+                        StringBuilder sbBuilder = new StringBuilder(encryptedMessage);
+                        sbBuilder.Replace(substring, replacement);
+                        encryptedMessage = sbBuilder.ToString();
+                    }
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
