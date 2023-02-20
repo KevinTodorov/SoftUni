d98be39e158aaeccd09f3edc4aade3cf01960924
Kevin Todorov <kevintodorov@Kevins-MBP.lan>
2023-02-20 23:05:17 +0200

    Added If-Else Lab From Softuni

---
+++ Programming Basics/If-else/AreaOfFigures/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if(type == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{side * side:f3}");
+            }else if(type == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideA * sideB:f3}");
+            }else if(type == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{r * r * Math.PI:f3}");
+            }else if(type == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideA * sideB / 2:f3}");
+            }
+        }
+    }
+}
