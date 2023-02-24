85f72da14494d5a124a57f8c6c39a940f25ccd2d
KevinTodorov <kevintodorov8@gmail.com>
2023-02-24 21:19:05 +0200

    Added Switch Lab

---
+++ Programming Basics/Switch copy/Fruit Shop/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            if(day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday") 
+            {
+                switch (name)
+                {
+                    case "banana":
+                        Console.WriteLine($"{(quantity * 2.5):f2}");
+                        break;
+                    case "apple":
+                        Console.WriteLine($"{(quantity * 1.2):f2}");
+                        break;
+                    case "orange":
+                        Console.WriteLine($"{(quantity * 0.85):f2}");
+                        break;
+                    case "grapefruit":
+                        Console.WriteLine($"{(quantity * 1.45):f2}");
+                        break;
+                    case "kiwi":
+                        Console.WriteLine($"{(quantity * 2.7):f2}");
+                        break;
+                    case "pineapple":
+                        Console.WriteLine($"{(quantity * 5.5):f2}");
+                        break;
+                    case "grapes":
+                        Console.WriteLine($"{(quantity * 3.85):f2}");
+                        break;
+                    default: Console.WriteLine("error");
+                        break;
+                }
+            }else if(day == "Saturday" || day == "Sunday")
+            {
+                switch (name)
+                {
+                    case "banana":
+                        Console.WriteLine($"{(quantity * 2.7):f2}");
+                        break;
+                    case "apple":
+                        Console.WriteLine($"{(quantity * 1.25):f2}");
+                        break;
+                    case "orange":
+                        Console.WriteLine($"{(quantity * 0.90):f2}");
+                        break;
+                    case "grapefruit":
+                        Console.WriteLine($"{(quantity * 1.60):f2}");
+                        break;
+                    case "kiwi":
+                        Console.WriteLine($"{(quantity * 3.00):f2}");
+                        break;
+                    case "pineapple":
+                        Console.WriteLine($"{(quantity * 5.6):f2}");
+                        break;
+                    case "grapes":
+                        Console.WriteLine($"{(quantity * 4.2):f2}");
+                        break;
+                    default: Console.WriteLine("error");
+                        break;
+                }
+            }
+            else Console.WriteLine("error");
+
+
+        }
+    }
+}
---
+++ Programming Basics/Switch copy/Small Shop/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Small_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double capacity = double.Parse(Console.ReadLine());
+
+            if (city == "Sofia")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        Console.WriteLine(capacity * 0.5);
+                        break;
+                    case "water":
+                        Console.WriteLine(capacity * 0.8);
+                        break;
+                    case "beer":
+                        Console.WriteLine(capacity * 1.2);
+                        break;
+                    case "sweets":
+                        Console.WriteLine(capacity * 1.45);
+                        break;
+                    case "peanuts":
+                        Console.WriteLine(capacity * 1.6);
+                        break;
+                }
+            } else if (city == "Plovdiv")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        Console.WriteLine(capacity * 0.4);
+                        break;
+                    case "water":
+                        Console.WriteLine(capacity * 0.7);
+                        break;
+                    case "beer":
+                        Console.WriteLine(capacity * 1.15);
+                        break;
+                    case "sweets":
+                        Console.WriteLine(capacity * 1.3);
+                        break;
+                    case "peanuts":
+                        Console.WriteLine(capacity * 1.5);
+                        break;
+                }
+
+            } else if (city == "Varna")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        Console.WriteLine(capacity * 0.45);
+                        break;
+                    case "water":
+                        Console.WriteLine(capacity * 0.7);
+                        break;
+                    case "beer":
+                        Console.WriteLine(capacity * 1.1);
+                        break;
+                    case "sweets":
+                        Console.WriteLine(capacity * 1.35);
+                        break;
+                    case "peanuts":
+                        Console.WriteLine(capacity * 1.55);
+                        break;
+                }
+            }
+            }
+        }
+    }
+
