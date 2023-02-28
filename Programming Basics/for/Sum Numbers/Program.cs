cbc9e49545f278c760324a7b3a395b6edc1f8f70
KevinTodorov <kevintodorov8@gmail.com>
2023-02-28 23:18:32 +0200

    Added For lab

---
+++ Programming Basics/for/Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i <= rows; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+                Console.WriteLine(sum);
+        }
+    }
+}
