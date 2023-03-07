21081ef2f6495a2f052da7a7911f59bc72f04230
KevinTodorov <kevintodorov8@gmail.com>
2023-03-07 17:07:32 +0200

    Added While exercise

---
+++ Programming Basics/while exercise/Cake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int pieces = width * lenght;
+            int untouchedPieces = pieces;
+            string input = Console.ReadLine();
+            
+            while(input != "STOP")
+            {
+
+                if(pieces < int.Parse(input))
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(pieces - int.Parse(input))} pieces more.");
+                    return;
+                }
+                pieces -= int.Parse(input);
+                input = Console.ReadLine();
+            }
+            if(untouchedPieces - pieces >= 0)
+            {
+                Console.WriteLine($"{pieces} pieces are left.");
+            }else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(untouchedPieces - pieces)} pieces more.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/while exercise/Exam Preparation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exam_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int badGrades = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int badGradesCounter = 0;
+            int counterProblems = 0;
+            string lastName = "";
+            double average = 0;
+
+            while(command != "Enough")
+            {
+                int grade = int.Parse(Console.ReadLine());
+                if(grade <= 4)
+                {
+                    badGradesCounter++;
+                }
+                if(badGradesCounter == badGrades)
+                {
+                    Console.WriteLine($"You need a break, {badGradesCounter} poor grades.");
+                    return;
+                }
+                counterProblems++;
+                average += grade;
+                lastName = command;
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Average score: {(average / counterProblems):f2}\r\nNumber of problems: {counterProblems}\r\nLast problem: {lastName}");
+        }
+    }
+}
