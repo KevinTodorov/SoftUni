85ef231ece6e3c3473eb7332c5f22283359549e7
KevinTodorov <kevintodorov8@gmail.com>
2023-03-13 18:37:08 +0200

    Added Programming Basics Exam

---
+++ Programming Basics/Programming Basics Online Exam - 6 and 7 April 2019/CinemaTickets/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CinemaTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filmName = Console.ReadLine();
+            double endCounter = 0;
+            double allTicketsCounter = 0;
+
+            double studentCounter = 0;
+            double standardCounter = 0;
+            double kidsCounter = 0;
+
+            double studentCounter2 = 0;
+            double standardCounter2 = 0;
+            double kidsCounter2 = 0;
+
+            while (filmName != "Finish")
+            {
+            int avaiblePlaces = int.Parse(Console.ReadLine());
+                string type = Console.ReadLine();
+                endCounter = 0;
+                studentCounter2 = 0;
+                standardCounter2 = 0;
+                kidsCounter2 = 0;
+
+                while (type != "End")
+                {
+                    endCounter++;
+                    allTicketsCounter++;
+
+                    switch (type)
+                    {
+                        case "student":
+                            studentCounter++;
+                            studentCounter2++;
+                            break;
+                        case "standard":
+                            standardCounter++;
+                            standardCounter2++;
+                            break;
+                        case "kid":
+                            kidsCounter++;
+                            kidsCounter2++;
+                            break;
+                    }
+                    if (endCounter >= avaiblePlaces)
+                    {
+                        break; ;
+                    }
+                    type = Console.ReadLine();
+                }
+                 double allSummed = studentCounter2 + standardCounter2 + kidsCounter2;
+                Console.WriteLine($"{filmName} - {((allSummed / avaiblePlaces) * 100):f2}% full.");
+                filmName = Console.ReadLine();
+            }
+            Console.WriteLine($"Total tickets: {allTicketsCounter}");
+            Console.WriteLine($"{((studentCounter / allTicketsCounter) * 100):f2}% student tickets.");
+            Console.WriteLine($"{((standardCounter / allTicketsCounter) * 100):f2}% standard tickets.");
+            Console.WriteLine($"{((kidsCounter / allTicketsCounter) * 100):f2}% kids tickets.");
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics Online Exam - 6 and 7 April 2019/CinemaVoucher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CinemaVoucher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voucher = int.Parse(Console.ReadLine());
+            string item = Console.ReadLine();
+
+            int filmTicketCounter = 0;
+            int otherThings = 0;
+
+            int sum = 0;
+            int price = 0;
+
+            while (item != "End")
+            {
+                int size = item.Length;
+
+                if (size > 8)
+                {
+                    char symbolOne = item[0];
+                    char symbolTwo = item[1];
+                    int one = symbolOne;
+                    int two = symbolTwo;
+                    price = one + two;
+                    if (price > voucher)
+                    {
+                        break;
+                    }
+                    voucher -= price;
+                    filmTicketCounter++;
+
+
+                }else if (size <= 8)
+                {
+                    char symbol = item[0];
+                    int n = symbol;
+                    if (n > voucher)
+                    {
+                        break;
+                    }
+                    voucher -= n;
+                    otherThings++;
+                }
+                item = Console.ReadLine();
+            }
+            Console.WriteLine(filmTicketCounter);
+            Console.WriteLine(otherThings);
+        }
+    }
+}
