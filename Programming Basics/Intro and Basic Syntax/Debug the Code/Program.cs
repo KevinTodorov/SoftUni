7a87c17d0b702c400827be06606bb2a0d63e9730
KevinTodorov <kevintodorov8@gmail.com>
2023-03-27 23:20:23 +0300

    Added Intro and Basic Syntax Fundamentals

---
+++ Programming Basics/Intro and Basic Syntax/Debug the Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Debug_the_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programming Basics/Intro and Basic Syntax/EvenNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+
+
+            while (flag != true)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (Math.Abs(n) % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {n}");
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+            }
+        }
+    }
+}
