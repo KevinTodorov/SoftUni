dec1a5eff1eefdb927bb0d844d627ceb4ea7a755
KevinTodorov <kevintodorov8@gmail.com>
2023-03-28 17:25:08 +0300

    Added Intro and basic syntax Exercise

---
+++ Programming Basics/First Steps In Programming /Exercise Intro and Basic Syntax/Rage Expenses/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double singleHeadsetPrice = double.Parse(Console.ReadLine());
+            double singleMousePrice = double.Parse(Console.ReadLine());
+            double singleKeyboardPrice = double.Parse(Console.ReadLine());
+            double singleDisplayPrice = double.Parse(Console.ReadLine());
+
+            int count = 0;
+            int brokenHeadsetCounter = 0;
+            int brokenMouseCounter = 0;
+            int brokenKeyboardCounter = 0;
+            int brokenDispayCounter = 0;
+
+            double sum = 0;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+
+                if (count % 2 == 0 && count != 0)
+                {
+                    brokenHeadsetCounter += 1;
+                    sum += singleHeadsetPrice;
+                }
+
+                if (count % 3 == 0 && count != 0)
+                {
+                    brokenMouseCounter += 1;
+                    sum += singleMousePrice;
+                }
+
+                if (count %  6 == 0 && count != 0)
+                {
+                    brokenKeyboardCounter += 1;
+                    sum += singleKeyboardPrice;
+
+                    if (count % 12 == 0)
+                    {
+                        brokenDispayCounter++;
+                        sum += singleDisplayPrice;
+                    }
+                }
+                count++;
+            }
+         
+            Console.WriteLine($"{sum:f2}");
+
+        }
+    }
+}
---
+++ Programming Basics/First Steps In Programming /Exercise Intro and Basic Syntax/VendingMachine/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double sum = 0;
+
+            while(command != "Start")
+            {
+
+                double coins = double.Parse(command);
+                sum += coins;
+
+                if (coins != 2 && coins != 1 && coins != 0.5 && coins != 0.2 && coins != 0.1)
+                {
+                    Console.WriteLine($"Cannot accept {coins}");
+                    sum -= coins;
+                }
+                command = Console.ReadLine();
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+
+                switch (input)
+                {
+                    case "Nuts":
+                        if (sum - 2 >= 0)
+                        {
+                            Console.WriteLine("Purchased nuts");
+                            sum -= 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Water":
+                        if (sum - 0.7 >= 0)
+                        {
+                            Console.WriteLine("Purchased water");
+                            sum -= 0.7;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Crisps":
+                        if (sum - 1.5 >= 0)
+                        {
+                            Console.WriteLine("Purchased crisps");
+                            sum -= 1.5;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Soda":
+                        if (sum - 0.8 >= 0)
+                        {
+                            Console.WriteLine("Purchased soda");
+                            sum -= 0.8;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Coke":
+                        if (sum - 1 >= 0)
+                        {
+                            Console.WriteLine("Purchased coke");
+                            sum -= 1;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                            break;
+
+                }
+                        input = Console.ReadLine();
+            }
+                Console.WriteLine($"Change: {sum:f2}");
+        }
+    }
+}
