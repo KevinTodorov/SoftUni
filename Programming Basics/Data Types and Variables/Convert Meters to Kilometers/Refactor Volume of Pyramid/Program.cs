2099d82ec2f0a0aadfac8727b295bff324484f49
KevinTodorov <kevintodorov8@gmail.com>
2023-03-29 19:02:54 +0300

    Added Data Types and Variables from Fundamentals

---
+++ Programming Basics/Data Types and Variables/Convert Meters to Kilometers/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Length: ");
+            Console.WriteLine($"Width: ");
+            Console.WriteLine($"Height: ");
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double volume = (lenght * width * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}
