80a9bb1ef5c1ef635629962ec2d9557793595b60
KevinTodorov <kevintodorov8@gmail.com>
2023-04-17 18:12:41 +0300

    Added List lab from Fundamentals

---
+++ Fundamentals/LIsts/ListManipulationBasics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string[] command = Console.ReadLine()
+                .Split();
+
+            while (command[0] != "end")
+            {
+                switch (command[0])
+                {
+                    case "Add":
+                        AddingMethod(numbers, command[1]);
+                        break;
+                    case "Remove":
+                        RemovingMethod(numbers, command[1]);
+                        break;
+                    case "RemoveAt":
+                        RemovingAtMethod(numbers, command[1]);
+                        break;
+                    case "Insert":
+                        InsertingMethod(numbers, command[1], command[2]);
+                        break;
+                }
+                command = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void InsertingMethod(List<int> numbers, string help, string help2)
+        {
+            string firstNum = help.ToString();
+            string secondNum = help2.ToString();
+            numbers.Insert(int.Parse(secondNum), int.Parse(firstNum));
+        }
+
+        private static void RemovingAtMethod(List<int> numbers, string help)
+        {
+            string numInStr = help.ToString();
+            numbers.RemoveAt(int.Parse(numInStr));
+        }
+
+        private static void RemovingMethod(List<int> numbers, string help)
+        {
+            string numInStr = help.ToString();
+            numbers.Remove(int.Parse(numInStr));
+        }
+
+        private static void AddingMethod(List<int> numbers, string help)
+        {
+            string numInStr = help.ToString();
+            numbers.Add(int.Parse(numInStr));
+        }
+    }
+}
---
+++ Fundamentals/LIsts/SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                double lastSum = 0;
+
+                if (i + 1 > numbers.Count - 1)
+                {
+                    break;
+                }
+
+                if (numbers[i] == numbers[i + 1])
+                {
+                    if (i > 0)
+                    {
+                      if (numbers[i - 1] == numbers[i] && numbers[i]== numbers[i + 1])
+                      {
+                        lastSum = numbers[i - 1] + numbers[i];
+                        numbers.RemoveAt(i - 1);
+                        numbers[i - 1] = lastSum;
+                        i = 0;
+                            i--;
+                        continue;
+                      }
+                    }
+                    lastSum = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i);
+                    numbers[i] = lastSum;
+                    i = 0;
+                    i--;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
