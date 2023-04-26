611619000cff8241e0c4e7e436c4a3f3d2c71c58
KevinTodorov <kevintodorov8@gmail.com>
2023-04-26 22:26:08 +0300

    Added Bitwise Operations From Fundamentals

---
+++ Fundamentals/Bitwise Operation/BinaryDigitsCount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            char oneOrZero = char.Parse(Console.ReadLine());
+            string binary = Convert.ToString(number, 2);
+            int count = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (oneOrZero == binary[i])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Fundamentals/Bitwise Operation/BitAtPosition1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace BitAtPosition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string numberInBits = Convert.ToString(number, 2);
+            BigInteger numberInInteger = BigInteger.Parse(numberInBits);
+
+            char n = ' ';
+
+            for (int i = numberInBits.Length - 1; i > 0; i--)
+            {
+                n = numberInBits[i - 1];
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
