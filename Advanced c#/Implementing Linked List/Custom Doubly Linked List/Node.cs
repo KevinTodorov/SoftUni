306a4123157b37b620a295d4063d6e44799b6c03
KevinTodorov <kevintodorov8@gmail.com>
2023-07-28 20:11:51 +0300

    Added Implementing Linked List

---
+++ Advanced c#/Implementing Linked List/Custom Doubly Linked List/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace Custom_Doubly_Linked_List
+{
+    /// <summary>
+    /// Creating Node class.
+    /// </summary>
+    public class Node<T>
+    {
+        /// <summary>
+        /// Creating property For Node Value.
+        /// </summary>
+        public T Value { get; set; }
+        /// <summary>
+        /// Creating property For Node Value.
+        /// </summary>
+        public Node<T> Next { get; set; }
+        /// <summary>
+        /// Creating property for Node Previous.
+        /// </summary>
+        public Node<T> Previous { get; set; }
+
+       
+    }
+}
---
+++ Advanced c#/Implementing Linked List/Custom Doubly Linked List/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Custom_Doubly_Linked_List;
+using System;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //This is primary List.
+            DoublyLinkedList<int> myList = new DoublyLinkedList<int>(new int[] {5, 7, 12, 13});
+            //You can put what you want here. You can try the methods for example.
+        }
+    }
+}
